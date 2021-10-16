f847391444fb90eff519128e8479e306a8ecf3c5
Documenter.jl <documenter@juliadocs.github.io>
2021-10-16 21:13:32 +0000

    build based on ab9e8e5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ExactConvolution","category":"page"},{"location":"#ExactConvolution","page":"Home","title":"ExactConvolution","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ExactConvolution.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ExactConvolution]","category":"page"}]
+}
